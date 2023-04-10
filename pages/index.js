dd79eea137ce46961cf44b37c2539c1f13dc5dfb
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-04-10 03:15:52 -0300

    improving style

--- pages/index.js
+++ pages/index.js
@@ -14,65 +14,70 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className='main'>
-        <header className='topbar'>
-          <h1 className='title' ><Image src={'/favicon.png'} width={62.25} height={58.5}></Image> Smart Health</h1>
-          <a className='entrar'>ENTRAR</a>
-        </header>
+      <div className='container'>
 
-        <main className='container'>
-          <div>
-            <h2 className={styles.description}>
-              Software para Gestão de Saúde!
-            </h2>
-            <p className={styles.description}>
-              Armazenamento, acompanhamento e compartilhamento de dados.
-            </p>
-          </div>
-          <div className={styles.infos}>
-            <div className={styles.textinfos}>
-              <h3 className={styles.funcionalidades}>
-                Nossas Funcionalidades
-              </h3>
-              <p className={styles.funcionalidades}>
-                <Image className={styles.funcionalidades} src={'/item.png'} width={15} height={22}></Image>
-                Acompanhamento de Antropometria Completa!
+        <div className='left-menu'></div>
+
+        <div className='main'>
+
+          <header className='topbar'>
+            <h1 className='title' ><Image src={'/favicon.png'} width={62.25} height={58.5}></Image>  <span className='displayMobile'>Smart Health</span></h1>
+            <a className='entrar'>ENTRAR</a>
+          </header>
+
+          <main className='content'>
+            <div className={styles.infos}>
+              <h2 className={styles.description}>
+                Software para Gestão de Saúde!
+              </h2>
+              <p className={styles.description}>
+                Armazenamento, acompanhamento e compartilhamento de dados.
               </p>
-              <p className={styles.funcionalidades}>
-                <Image className={styles.funcionalidades} src={'/item.png'} width={15} height={22}></Image>
-                Acompanhamento de Exames Médicos!
-              </p>
-              <p className={styles.funcionalidades}>
-                <Image className={styles.funcionalidades} src={'/item.png'} width={15} height={22}></Image>
-                Gestão de Exames Alterados!
-              </p>
-              <p className={styles.funcionalidades}>
-                <Image className={styles.funcionalidades} src={'/item.png'} width={15} height={22}></Image>
-                Facilidade no Upload de Exames Médicos!
-              </p>
-              <p className={styles.funcionalidades}>
-                <Image className={styles.funcionalidades} src={'/item.png'} width={15} height={22}></Image>
-                Disponibilidade de Compartilhamento de dados!
-              </p>
+              <div className={styles.textinfos}>
+                <h3 className={styles.funcionalidades}>
+                  Nossas Funcionalidades
+                </h3>
+                <p className={styles.funcionalidades}>
+                  <Image className={styles.funcionalidades} src={'/item.png'} width={15} height={22}></Image>
+                  Acompanhamento de Antropometria Completa!
+                </p>
+                <p className={styles.funcionalidades}>
+                  <Image className={styles.funcionalidades} src={'/item.png'} width={15} height={22}></Image>
+                  Acompanhamento de Exames Médicos!
+                </p>
+                <p className={styles.funcionalidades}>
+                  <Image className={styles.funcionalidades} src={'/item.png'} width={15} height={22}></Image>
+                  Gestão de Exames Alterados!
+                </p>
+                <p className={styles.funcionalidades}>
+                  <Image className={styles.funcionalidades} src={'/item.png'} width={15} height={22}></Image>
+                  Facilidade no Upload de Exames Médicos!
+                </p>
+                <p className={styles.funcionalidades}>
+                  <Image className={styles.funcionalidades} src={'/item.png'} width={15} height={22}></Image>
+                  Disponibilidade de Compartilhamento de dados!
+                </p>
+              </div>
             </div>
+
             <div className={styles.images}>
               <Image className={styles.image1} src={'/background.png'} width={500} height={500}></Image>
             </div>
-          </div>
-        </main>
 
-        <footer className='footer'>
-          <p>
-            <FontAwesomeIcon className='icon' icon={faInstagram} />
-            <FontAwesomeIcon className='icon' icon={faFacebookSquare} />
-            <FontAwesomeIcon className='icon' icon={faLinkedin} />
-          </p>
-          <a className='ajuda'>
-            <FontAwesomeIcon icon={faQuestionCircle} /> Posso ajudar?
-          </a>
-        </footer>
+          </main>
 
-        <div className='left-menu'></div>
+          <footer className='footer'>
+            <p>
+              <FontAwesomeIcon className='icon' icon={faInstagram} />
+              <FontAwesomeIcon className='icon' icon={faFacebookSquare} />
+              <FontAwesomeIcon className='icon' icon={faLinkedin} />
+            </p>
+            <a className='ajuda'>
+              <FontAwesomeIcon icon={faQuestionCircle} /> <span className='displayMobile'>Posso ajudar?</span>
+            </a>
+          </footer>
+
+        </div>
       </div>
     </>
   )
