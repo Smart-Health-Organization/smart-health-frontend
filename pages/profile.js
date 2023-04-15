3d1d63404c2d8ee4960ddb4b6084395885b81ce8
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-04-15 20:43:45 -0300

    Defining profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,7 @@
 import LeftMenu from '@/components/leftMenu'
 import MobileMenu from '@/components/mobileMenu'
 import Head from 'next/head'
+import styles from '@/styles/Profile.module.css'
 import { useEffect, useState } from 'react'
 
 export default function Home() {
@@ -28,7 +29,7 @@ export default function Home() {
       <div className='container'>
 
         <LeftMenu></LeftMenu>
- 
+
         <div className='main authPage' style={{ justifyContent: 'flex-start' }}>
 
           <header className='topbar'>
@@ -37,8 +38,35 @@ export default function Home() {
             <h1 className='title time'>{date}</h1>
           </header>
 
-          <main className='content' style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <main className='content' style={{ justifyContent: 'flex-start', alignItems: 'flex-start', flexDirection: 'column' }}>
+            <h1>Configuração - Meu perfil</h1>
 
+            <h2 className={styles.userInfos} id='userName'>Nome Sobrenome</h2>
+            <p className={styles.userInfos} id='userEmail'>email@mail.com</p>
+            <div className={styles.userInfos}>
+              <div>
+                <label><input readOnly value={0} type='number'></input> anos</label>
+                <select>
+                  <option value=''>Selecione o sexo</option>
+                  {/* <option value='masculino'>Masculino</option>
+                  <option value='feminino'>Feminino</option> */}
+                </select>
+              </div>
+              <div>
+                <label><input readOnly value={75} type='number'></input> kg</label>
+                <label><input readOnly value={185} type='number'></input> cm</label>
+              </div>
+            </div>
+
+            <div className={styles.userInfos}>
+              <div className={styles.changePassword}>
+                <input placeholder='Senha atual' type='password'></input>
+                <input placeholder='Nova senha' type='password'></input>
+              </div>
+            </div>
+            <a className='ajuda'>
+              Trocar senha
+            </a>
           </main>
         </div>
       </div>
