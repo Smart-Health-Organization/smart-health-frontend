7000e8172e8e6e8cce87758f2f0b28ec32ace708
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-04-22 02:39:45 -0300

    Connecting the register page to backend

--- pages/register.js
+++ pages/register.js
@@ -3,8 +3,70 @@ import styles from '@/styles/Login.module.css'
 import Image from 'next/image'
 import Link from 'next/link';
 import DateComponent from '@/components/date';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import SnackBar from '@/components/SnackBar';
+import Loading from '@/components/loading';
 
 export default function Register() {
+
+  const [errorMessages, setErrorMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    document.querySelector("#name").focus();
+  });
+
+  function onEnter(e) {
+    if (e.key != 'Enter') return;
+
+    if (!e.target.nextElementSibling) {
+      register();
+      return;
+    }
+
+    e.target.nextElementSibling.focus();
+  }
+
+  async function register() {
+    if (isLoading) return;
+
+    setErrorMessages([]);
+
+    setIsLoading(true);
+
+    const newUser = {
+      name: document.querySelector("#name").value,
+      age: Number(document.querySelector("#age").value),
+      sexo: document.querySelector("#sexo").value,
+      email: document.querySelector("#email").value,
+      login: document.querySelector("#login").value,
+      password: document.querySelector("#password").value,
+    }
+
+    try {
+      await axios.post(process.env.NEXT_PUBLIC_API_URL + '/signup', newUser);
+
+      const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + '/login', { email: newUser.email, password: newUser.password });
+
+      sessionStorage.setItem("token", "Bearer " + response.data.token);
+
+      window.location.replace("/profile");
+    }
+    catch (error) {
+      if (!error.response.data.message.map) {
+        setErrorMessages([<li>{error.response.data.message}</li>]);
+      }
+      else {
+        setErrorMessages(error.response.data.message.map((message, index) => {
+          return <li key={index}>{message}</li>
+        }));
+      }
+    }
+    finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <>
@@ -14,6 +76,10 @@ export default function Register() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
+
+      <SnackBar option={"warning"} message={errorMessages}></SnackBar>
+      <Loading on={isLoading}></Loading>
+
       <div className='container'>
 
         <div className='main' style={{ justifyContent: 'flex-start' }}>
@@ -43,14 +109,19 @@ export default function Register() {
                 <p className={styles.description}>
                   Preencha o formulário
                 </p>
-                <form className={styles.formLogin} style={{ height: '60%' }}>
-                  <input type='text' placeholder={"Nome"}></input>
-                  <input type='number' placeholder={"Idade"}></input>
-                  <input type='email' placeholder={"Email"}></input>
-                  <input type='text' placeholder={"Login"}></input>
-                  <input type='password' placeholder={"Senha"}></input>
+                <form className={styles.formLogin} style={{ height: '70%' }}>
+                  <input onKeyDown={onEnter} id='name' type='text' placeholder={"Nome"}></input>
+                  <input onKeyDown={onEnter} id='age' type='number' placeholder={"Idade"}></input>
+                  <select onKeyDown={onEnter} id='sexo' >
+                    <option value=''>Selecione o sexo</option>
+                    <option value='masculino'>Masculino</option>
+                    <option value='feminino'>Feminino</option>
+                  </select>
+                  <input onKeyDown={onEnter} id='email' type='email' placeholder={"Email"}></input>
+                  <input onKeyDown={onEnter} id='login' type='text' placeholder={"Login"}></input>
+                  <input onKeyDown={onEnter} id='password' type='password' placeholder={"Senha"}></input>
                 </form>
-                <a>Cadastrar</a>
+                <button disabled={isLoading} onClick={register}>Cadastrar</button>
               </div>
             </div>
           </main>
