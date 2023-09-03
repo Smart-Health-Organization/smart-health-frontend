156c9d92b7070616de68258fd97913fb4a7dfa99
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-09-02 22:25:27 -0300

    tela "ver-antropometria" por data

--- pages/ver-antropometrias.js
+++ pages/ver-antropometrias.js
@@ -1,67 +1,82 @@
 import LeftMenu from '@/components/LeftMenuComponent'
 import Head from 'next/head'
-import styles from '@/styles/Exams.module.css'
+import styles from '@/styles/AllMetas.module.css'
+import styles1 from '@/styles/Exams.module.css'
 import { useEffect, useState } from 'react'
 import SnackBar from '@/components/SnackBarComponent'
 import Loading from '@/components/LoadingComponent'
 import axios from 'axios'
 import tryLogin from '@/functions/tryLogin'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDna } from '@fortawesome/free-solid-svg-icons'
-import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons'
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    Filler
-} from 'chart.js'
-import { Line } from 'react-chartjs-2'
+import { faCalendar } from '@fortawesome/free-regular-svg-icons'
 import TopBar from '@/components/TopBar'
 import Link from 'next/link'
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    Filler
-);
 
 export default function Antropometrias() {
     const [errorMessages, setErrorMessages] = useState([]);
     const [typeOfMessage, setTypeOfMessage] = useState('warning');
     const [isLoading, setIsLoading] = useState(true);
 
-    const [exames, setExames] = useState([]);
-    const [examesBuscados, setExamesBuscados] = useState([]);
+    const [antropometrias, setAntropometrias] = useState([]);
 
     useEffect(() => {
         tryLogin(setIsLoading, axios, false);
-        setIsLoading(true);
-        getExames(setErrorMessages, setIsLoading, setTypeOfMessage).then((response) => setExames(response));
+
+        getAllAntropometrias(setIsLoading).then((response) => {
+            let todasAntropometrias = [];
+
+            console.log(antropometrias);
+
+            response.map(data => {
+                todasAntropometrias.push(
+                    <div key={data.id} className={styles.meta} style={{marginTop: '0'}}>
+                        <div className={styles.left}>
+                            <h3>Medição#{data.id}</h3>
+                            <p><FontAwesomeIcon icon={faCalendar} /> Feito em: {(new Date(data.data)).toLocaleDateString('pt-BR', { timeZone: 'UTC' })}</p>
+                        </div>
+                        <div className={styles.right} style={{justifyContent: 'center'}}>
+                            <button onClick={() => deleteAntropometria(data.id)} className='ajuda delete'> Remover</button>
+                        </div>
+                    </div>
+                )
+            });
+
+            setAntropometrias(todasAntropometrias);
+        });
     }, []);
 
-    useEffect(() => {
-        search('');
-    }, [exames]);
+    async function deleteAntropometria(id) {
+        if (!confirm('Tem certeza que deseja deletar essa medição?')) return;
 
-    function search(string) {
-        if (string === '') {
-            setExamesBuscados(exames);
-            return;
+        setIsLoading(true);
+
+        try {
+            await axios.delete(process.env.NEXT_PUBLIC_API_URL + '/usuarios/' + sessionStorage.getItem('user') + '/metas/' + sessionStorage.getItem('meta') + '/antropometrias/' + id, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': sessionStorage.getItem('token')
+                }
+            });
+
+            setTypeOfMessage('success');
+            setErrorMessages([<li key={0}>Medição removida com sucesso!</li>]);
+            setTimeout(() => window.location.href = '/ver-antropometrias', 1000);
         }
-
-        const searchedExams = exames.filter((exam) => exam.key.toUpperCase().startsWith(string.toUpperCase()));
-        setExamesBuscados(searchedExams);
-        return;
+        catch (error) {
+            setTypeOfMessage('warning');
+            if (error.response)
+                if (!error.response.data.message.map) {
+                    setErrorMessages([<li key={0}>{error.response.data.message}</li>]);
+                }
+                else {
+                    setErrorMessages(error.response.data.message.map((message, index) => {
+                        return <li key={index}>{message}</li>
+                    }));
+                }
+        }
+        finally {
+            setIsLoading(false);
+        }
     }
 
     return (
@@ -92,18 +107,17 @@ export default function Antropometrias() {
 
             <div className='container'>
 
-                <LeftMenu actualpage='/ver-exames'></LeftMenu>
+                <LeftMenu actualpage='/acompanhar-meta'></LeftMenu>
 
                 <div className='main authPage'>
 
-                    <TopBar actualpage='/ver-exames'></TopBar>
-
+                    <TopBar actualpage='/acompanhar-meta'></TopBar>
 
                     <main className='content' style={{ justifyContent: 'flex-start', alignItems: 'flex-start', flexDirection: 'column', marginBottom: '25px' }}>
-                        <div className={styles.exams}>
+                        <div className={styles1.exams}>
                             <h2 className='subtitle' style={{ marginBottom: "30px" }}>Minhas medições</h2>
-                            <h2>Em breve!</h2>
-                            <div className={[styles.card_chart, styles.novosexames].join(" ")}>
+                            {antropometrias}
+                            <div className={[styles1.card_chart, styles1.novosexames].join(" ")}>
                                 <p>
                                     Adicione uma nova medição para acompanhar sua evolução.
                                 </p>
@@ -118,75 +132,16 @@ export default function Antropometrias() {
     )
 }
 
-export async function getExames(setErrorMessages, setIsLoading, setTypeOfMessage) {
-    try {
-        const response = await axios.get(process.env.NEXT_PUBLIC_API_URL + '/usuarios/' + sessionStorage.getItem("user") + '/exame-itens',
-            { headers: { Authorization: sessionStorage.getItem("token") } });
+async function getAllAntropometrias(setIsLoading) {
+    setIsLoading(true);
 
-        let examesLista = [];
-        const dados = response.data.data;
+    const response = await axios.get(process.env.NEXT_PUBLIC_API_URL + '/usuarios/' + sessionStorage.getItem('user') + '/metas/' + sessionStorage.getItem('meta') + '/antropometrias', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': sessionStorage.getItem('token')
+        }
+    });
 
-        Object.keys(dados).forEach((examitem) => {
-            let dadosExames = [...dados[examitem]];
-            dadosExames = dadosExames.reverse();
-
-            const dadosGraficoExame = {
-                labels: dadosExames.map(row => new Date(row.data).toLocaleDateString()),
-                datasets: [
-                    {
-                        label: examitem + ' - ' + dadosExames[0].unidade,
-                        data: dadosExames.map(row => row.medida),
-                        borderColor: "#F3A53F",
-                        backgroundColor: "rgba(248, 222, 189, 0.6)",
-                        borderWidth: 4,
-                        fill: true,
-                    }
-                ]
-            };
-
-            const configuracaoGraficoExame = {
-                cubicInterpolationMode: 'monotone',
-                plugins: {
-                    legend: {
-                        display: false,
-                    },
-                    tooltip: {
-                        enabled: true,
-                    }
-                },
-                maintainAspectRatio: false,
-            }
-
-            examesLista.push(<div className={styles.card_chart} key={examitem}>
-                <div className={styles.chart_info}>
-                    <h3><FontAwesomeIcon icon={faDna} /> {examitem}</h3>
-                    <h3 className={styles.gray}>ATUAL</h3>
-                    <h2>{dados[examitem][0].medida} <span className={[styles.gray, styles.umedida].join(" ")}>{dados[examitem][0].unidade}</span> <span data-isalterado={dados[examitem][0].isAlterado} className={styles.isAlterado}>{dados[examitem][0].isAlterado ? "Alterado" : "Normal"} <span className={styles.tooltip}><FontAwesomeIcon icon={faCircleQuestion} /> <span className={styles.tooltiptext}>De acordo com o Ministério da Saúde.</span></span></span></h2>
-                </div>
-                <div className={styles.chart_container}>
-                    <Line
-                        data={dadosGraficoExame}
-                        options={configuracaoGraficoExame}
-                    />
-                </div>
-            </div>);
-        });
-
-        return examesLista;
-    }
-    catch (error) {
-        setTypeOfMessage('warning');
-        if (error.response)
-            if (!error.response.data.message.map) {
-                setErrorMessages([<li key={0}>{error.response.data.message}</li>]);
-            }
-            else {
-                setErrorMessages(error.response.data.message.map((message, index) => {
-                    return <li key={index}>{message}</li>
-                }));
-            }
-    }
-    finally {
-        setIsLoading(false);
-    }
+    setIsLoading(false);
+    return response.data;
 }
