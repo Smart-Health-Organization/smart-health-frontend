6008a462965c6d204c2b7a2f367c57d750a75e3d
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-09-09 09:48:58 -0300

    completando testes no snackBar

--- __tests__/SnackBarComponent-test.js
+++ __tests__/SnackBarComponent-test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import SnackBarComponent from '../components/SnackBarComponent';
 
 describe('SnackBarComponent', () => {
@@ -30,7 +30,32 @@ describe('SnackBarComponent', () => {
         expect(returnElement).not.toBeInTheDocument();
     });
 
-    //todo: testar estilo
-    //todo: testar fadingOut
-    //todo: testar useEffect
+    it('SnackBarComponent must have a class named "snackbar"', () => {
+        // 1. renderizar o componente passando suas props
+        const renderredComponent = render(<SnackBarComponent message={[<li key={0}>Info Message</li>]} option='info' />,);
+
+        // 2. pegar o elemento que será testado
+        const returnElement = renderredComponent.container.firstChild;
+
+        // 3. Aplicar a ação que será testada
+        // nada a fazer
+
+        // 4. fazer o teste
+        expect(returnElement).toHaveClass('snackbar');
+    });
+
+    it('SnackBarComponent faded out after 5000', () => {
+        // 1. renderizar o componente passando suas props
+        const renderredComponent = render(<SnackBarComponent message={[<li key={0}>Info Message</li>]} option='info' />,);
+
+        // 2. pegar o elemento que será testado
+        const returnElement = renderredComponent.container.firstChild;
+
+        // 3. Aplicar a ação que será testada
+        // nada a fazer
+
+        // 4. fazer o teste
+        waitFor(() => expect(returnElement).toHaveClass('snackbarFadeOut'), { interval: 5000, timeout: 5000 });
+    });
+
 });
